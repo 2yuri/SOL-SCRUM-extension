35ebb98aa398e72064886a14b5885ff215d2e45a
hyperyuri <yuri.snp@hotmail.com>
2020-08-11 10:24:56 -0400

    validation in add image

--- app.js
+++ app.js
@@ -12,17 +12,24 @@ input.style.marginLeft = '10px';
 //BUTTON
 const button = document.createElement('BUTTON');
 button.textContent = 'ENVIAR'
+button.style.cursor = 'pointer'
 div.appendChild(button)
 
 
-function updateImageAtBackground(){
+function updateImageAtBackground() {
   const url = input.value;
-  document.body.style.backgroundImage = `url(${url})`;
-  localStorage.setItem('UrlBackground', JSON.stringify(url));
-  
-  input.value = ''
 
-  alert('Imagem alterada com Sucesso!')
+  if (url.includes('http')) {
+    document.body.style.backgroundImage = `url(${url})`;
+    localStorage.setItem('UrlBackground', JSON.stringify(url));
+
+    input.value = ''
+
+    alert('Imagem alterada com Sucesso!')
+  } else {
+    alert('Digite uma URL valida!')
+  }
+
 }
 
 
@@ -32,7 +39,7 @@ button.onclick = updateImageAtBackground;
 //IMAGE BACKGROUND
 const receive = localStorage.getItem('UrlBackground');
 
-if (receive !== '' && receive !== undefined){  
+if (receive !== '' && receive !== undefined) {
   document.body.style.backgroundImage = `url(${JSON.parse(receive)})`;
 }
 
